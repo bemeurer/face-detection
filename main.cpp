d38b07df9963d2cbafc5376488c0f10a8e63681a
Bernardo Meurer <meurerbernardo@gmail.com>
2016-07-21 18:12:30 -0300

    Deleted old, unused, code

--- main.cpp
+++ main.cpp
@@ -3,12 +3,6 @@
 
 using namespace cv;
 
-std::vector<Rect> detectFace(CascadeClassifier faceCascade, Mat frame_gray);
-
-std::vector<Rect> detectNose(CascadeClassifier noseCascade, Mat face_gray);
-
-std::vector<Rect> detectSmile(CascadeClassifier smileCascade, Mat lower_face);
-
 int main() {
     VideoCapture vid(0);
     if (!vid.isOpened()) {
@@ -16,10 +10,8 @@ int main() {
     }
     namedWindow("stream", WINDOW_AUTOSIZE);
     CascadeClassifier faceCascade;
-    CascadeClassifier noseCascade;
     CascadeClassifier smileCascade;
     faceCascade.load("cascades/frontal_face.xml");
-    noseCascade.load("cascades/nose.xml");
     smileCascade.load("cascades/smile.xml");
     Mat frame;
     while (true) {
@@ -37,7 +29,7 @@ int main() {
             int half = faces[i].y + int(faces[i].height * 0.6);
             Rect lower_face(faces[i].x, half, faces[i].width, (faces[i].y + faces[i].height) - half);
 
-            //XXX: There are here for debugging
+            //XXX: These are here for debugging
             //rectangle(frame, lower_face, CV_RGB(0,0,0), 7);
             line(frame, cvPoint(0, half), cvPoint(frame_gray.cols, half), CV_RGB(0, 0, 0), 2);
 
@@ -46,7 +38,7 @@ int main() {
                                           Size(10, 10));
             for (size_t x = 0; x < smile.size(); x++) {
                 Rect smile_r(faces[i].x + smile[x].x, half + smile[x].y, smile[x].width, smile[x].height);
-                rectangle(frame, smile_r, CV_RGB(255, 0, 0));
+                rectangle(frame, smile_r, CV_RGB(0, 255, 0), 3);
             }
         }
         imshow("stream", frame);
